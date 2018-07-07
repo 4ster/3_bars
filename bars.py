7561b2c1d69a201ce2a9c149ee3c3e17ebf1a3d6
Andrey Tokarev <abtokarev@gmail.com>
2018-07-07 21:57:21 +0300

    Output reworked

--- bars.py
+++ bars.py
@@ -3,47 +3,56 @@ from math import sqrt
 
 
 def load_data(filepath):
-    with open(filepath) as f:
-        bars_list = json.load(f)
+    with open(filepath) as json_file:
+        bars_list = json.load(json_file)
     return bars_list
 
 
 def get_biggest_bar(bars_list):
-    top_biggest = max(bars_list["features"],
-                      key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
-    return top_biggest["properties"]["RowId"]
+    features = bars_list["features"]
+    top_biggest_bar = max(
+        features,
+        key=lambda x: x["properties"]["Attributes"]["SeatsCount"]
+    )
+    return top_biggest_bar
 
 
 def get_smallest_bar(bars_list):
-    top_smallest = min(bars_list["features"],
-                       key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
-    return top_smallest["properties"]["RowId"]
+    features = bars_list["features"]
+    top_smallest_bar = min(
+        features,
+        key=lambda x: x["properties"]["Attributes"]["SeatsCount"]
+    )
+    return top_smallest_bar
 
 
 def get_closest_bar(bars_list, longitude, latitude):
-    top_closest = min(bars_list["features"],
-                      key=lambda x: (x["geometry"]["coordinates"][0] - longitude)**2 + x["geometry"]["coordinates"][1] - latitude**2)
-    return top_closest["properties"]["RowId"]
+    features = bars_list["features"]
+    top_closest_bar = min(
+        features,
+        key=lambda x: (
+            x["geometry"]["coordinates"][0] - longitude)**2
+        + x["geometry"]["coordinates"][1] - latitude**2
+    )
+    return top_closest_bar
 
-
-def get_bar_name(bars_list, row_id):
-    # Return name of bar with given RowId
-    for b in bars_list["features"]:
-        if(b["properties"]["RowId"] == row_id):
-            return b["properties"]["Attributes"]["Name"]
+def print_bar(message, bar):
+    print("{0} \"{1}\"".format(
+        message,
+        bar["properties"]["Attributes"]["Name"]
+    ))
 
 
 if __name__ == '__main__':
     bars_list = load_data('bars.json')
-    biggest_id = get_biggest_bar(bars_list)
-    smallest_id = get_smallest_bar(bars_list)
+    biggest_bar = get_biggest_bar(bars_list)
+    smallest_bar = get_smallest_bar(bars_list)
 
     latitude = float(input("Input latitude:"))
     longitude = float(input("Input longitude:"))
 
-    closest_id = get_closest_bar(bars_list, longitude, latitude)
+    closest_bar = get_closest_bar(bars_list, longitude, latitude)
 
-    print("Biggest bar is \"{0}\"".format(get_bar_name(bars_list, biggest_id)))
-    print("Smallest bar is \"{0}\"".format(
-        get_bar_name(bars_list, smallest_id)))
-    print("Closest bar is \"{0}\"".format(get_bar_name(bars_list, closest_id)))
+    print_bar("Biggest bar is", biggest_bar)
+    print_bar("Smallest bar is", smallest_bar)
+    print_bar("Closest bar is", closest_bar)
