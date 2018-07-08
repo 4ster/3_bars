f111d76d9a0a00a4d0cf3e99a03216c963757fa2
Andrey Tokarev <abtokarev@gmail.com>
2018-07-08 10:15:21 +0300

    Refactor repeating code

--- bars.py
+++ bars.py
@@ -10,27 +10,24 @@ def load_data(filepath):
 
 
 def get_biggest_bar(bars_list):
-    features = bars_list["features"]
     top_biggest_bar = max(
-        features,
+        bars,
         key=lambda x: x["properties"]["Attributes"]["SeatsCount"]
     )
     return top_biggest_bar
 
 
 def get_smallest_bar(bars_list):
-    features = bars_list["features"]
     top_smallest_bar = min(
-        features,
+        bars,
         key=lambda x: x["properties"]["Attributes"]["SeatsCount"]
     )
     return top_smallest_bar
 
 
 def get_closest_bar(bars_list, longitude, latitude):
-    features = bars_list["features"]
     top_closest_bar = min(
-        features,
+        bars,
         key=lambda x: (
             x["geometry"]["coordinates"][0] - longitude)**2
         + x["geometry"]["coordinates"][1] - latitude**2
@@ -58,7 +55,7 @@ def create_parser():
 if __name__ == '__main__':
     args = create_parser()
     bars_list = load_data(args.filepath)
-
+    bars = bars_list["features"]
     biggest_bar = get_biggest_bar(bars_list)
     smallest_bar = get_smallest_bar(bars_list)
     while True:
